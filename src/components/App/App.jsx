bebd5540d4f62178a69095bd1342ee772520f94b
BohdanChurevych <bohdan.churevych@gmail.com>
2023-04-03 00:00:03 +0300

    done

---
+++ src/components/App/App.jsx
@@ -0,0 +1,71 @@
+import { Component } from 'react';
+
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactsList from '../ContactsList/ContaactsList';
+import Filter from '../Filter/Filter';
+import { Container } from './App.styled';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  formSubmitHandler = data => {
+    this.state.contacts.find(
+      ({ name }) => name.toLowerCase() === data.name.toLowerCase()
+    )
+      ? alert(`${data.name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [data, ...prevState.contacts],
+        }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFiltredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContacts = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} contacts={contacts} />
+
+        <h2>Contacts</h2>
+        <Filter changeFilter={this.changeFilter} filter={filter} />
+        <ContactsList
+          contacts={this.getFiltredContacts()}
+          onDeleteContact={this.deleteContacts}
+        />
+      </Container>
+    );
+  }
+}
+export default App;
